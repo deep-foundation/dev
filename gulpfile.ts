9a8f86d3610592bb775b21ed218aa670756f25d1
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-13 14:55:45 +0000

    gulp-util to minimist

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,20 +1,33 @@
 
 import url from 'url';
 import del from 'del';
-import util from 'gulp-util';
+import fs from 'fs';
+import concurrently from 'concurrently';
+import minimist from 'minimist';
 import * as gulp from 'gulp';
 import Git from 'simple-git/promise';
+
 const git = Git();
+const argv = require('minimist')(process.argv.slice(2));
 
 gulp.task('assets:update', () => (
   gulp.src('assets/*', { base: 'assets' }).pipe(gulp.dest('./applications/'))
 ));
 
 gulp.task('package:insert', async () => {
-  await git.submoduleAdd(util.env.url, `packages/${util.env.name}`);
+  await git.submoduleAdd(argv.url, `packages/${argv.name}`);
 });
 
 gulp.task('package:delete', async () => {
-  await del([`packages/${util.env.name}`, `.git/modules/packages/${util.env.name}`]);
-  await git.rm(`packages/${util.env.name}`);
+  await del([`packages/${argv.name}`, `.git/modules/packages/${argv.name}`]);
+  await git.rm(`packages/${argv.name}`);
+});
+
+gulp.task('packages:update', async () => {
+  await git.submoduleUpdate();
+  const packages = fs.readdirSync(`${__dirname}/packages`);
+  for (let p in packages) {
+    const pckg = packages[p];
+    await concurrently([`(cd ${__dirname}/packages/${pckg} && npm i)`]);
+  }
 });
