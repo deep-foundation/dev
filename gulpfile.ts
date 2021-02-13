cb0f016cde9d0cec8ce15f9c0a0c6be2f75bdd43
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-13 13:48:47 +0000

    base submodule tasks and store package

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,6 @@
 
 import url from 'url';
+import del from 'del';
 import util from 'gulp-util';
 import * as gulp from 'gulp';
 import Git from 'simple-git/promise';
@@ -9,6 +10,11 @@ gulp.task('assets:update', () => (
   gulp.src('assets/*', { base: 'assets' }).pipe(gulp.dest('./applications/'))
 ));
 
-gulp.task('package:add', async () => {
+gulp.task('package:insert', async () => {
   await git.submoduleAdd(util.env.url, `packages/${util.env.name}`);
+});
+
+gulp.task('package:delete', async () => {
+  await del([`packages/${util.env.name}`, `.git/modules/packages/${util.env.name}`]);
+  await git.rm(`packages/${util.env.name}`);
 });
