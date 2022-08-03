1d2c90a5e2bf2e4f40229b802f31d343a4cccc93
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-03 18:35:06 +0600

    Do not import. "Hardcode"

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -3,16 +3,6 @@ require('graphql');
 require('lodash');
 require('subscriptions-transport-ws');
 require('dotenv').config();
-const { init } = require('./tinkoff-split/init');
-const {
-	getUrl,
-	getError,
-	_generateToken,
-	generateToken,
-	errorsConverter,
-	generateTestToken,
-	sleep,
-} = require('./tinkoff-split/_utils');
 const { generateApolloClient } = require('@deep-foundation/hasura/client');
 const { DeepClient } = require('@deep-foundation/deeplinks/imports/client');
 const {
@@ -120,6 +110,123 @@ const f = async () => {
 
 	const unloginedDeep = new DeepClient({ apolloClient });
 
+	const errorsConverter = {
+		7:	'Покупатель не найден',
+		53:	'Обратитесь к продавцу',
+		99:	'Платеж отклонен',
+		100:	'Повторите попытку позже',
+		101:	'Не пройдена идентификация 3DS',
+		102:	'Операция отклонена, пожалуйста обратитесь в интернет-магазин или воспользуйтесь другой картой',
+		103:	'Повторите попытку позже',
+		119:	'Превышено кол-во запросов на авторизацию',
+		191:	'Некорректный статус договора, обратитесь к вашему менеджеру',
+		1001:	'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+		1003:	'Неверный merchant ID',
+		1004:	'Карта украдена. Свяжитесь с банком, выпустившим карту',
+		1005:	'Платеж отклонен банком, выпустившим карту',
+		1006:	'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+		1007:	'Карта украдена. Свяжитесь с банком, выпустившим карту',
+		1008:	'Платеж отклонен, необходима идентификация',
+		1012:	'Такие операции запрещены для этой карты',
+		1013:	'Повторите попытку позже',
+		1014:	'Карта недействительна. Свяжитесь с банком, выпустившим карту',
+		1015:	'Попробуйте снова или свяжитесь с банком, выпустившим карту',
+		1019:	'Платеж отклонен — попробуйте снова',
+		1030:	'Повторите попытку позже',
+		1033:	'Истек срок действия карты. Свяжитесь с банком, выпустившим карту',
+		1034:	'Попробуйте повторить попытку позже',
+		1038:	'Превышено количество попыток ввода ПИН-кода',
+		1039:	'Платеж отклонен — счет не найден',
+		1041:	'Карта утеряна. Свяжитесь с банком, выпустившим карту',
+		1043:	'Карта украдена. Свяжитесь с банком, выпустившим карту',
+		1051:	'Недостаточно средств на карте',
+		1053:	'Платеж отклонен — счет не найден',
+		1054:	'Истек срок действия карты',
+		1055:	'Неверный ПИН',
+		1057:	'Такие операции запрещены для этой карты',
+		1058:	'Такие операции запрещены для этой карты',
+		1059:	'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+		1061:	'Превышен дневной лимит платежей по карте',
+		1062:	'Платежи по карте ограничены',
+		1063:	'Операции по карте ограничены',
+		1064:	'Проверьте сумму',
+		1065:	'Превышен дневной лимит транзакций',
+		1075:	'Превышено число попыток ввода ПИН-кода',
+		1076:	'Платеж отклонен — попробуйте снова',
+		1077:	'Коды не совпадают — попробуйте снова',
+		1080:	'Неверный срок действия',
+		1082:	'Неверный CVV',
+		1086:	'Платеж отклонен — не получилось подтвердить ПИН-код',
+		1088:	'Ошибка шифрования. Попробуйте снова',
+		1089:	'Попробуйте повторить попытку позже',
+		1091:	'Банк, выпустивший карту недоступен для проведения авторизации',
+		1092:	'Платеж отклонен — попробуйте снова',
+		1093:	'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+		1094:	'Системная ошибка',
+		1096:	'Повторите попытку позже',
+		9999:	'Внутренняя ошибка системы',
+	};
+
+	const getError = errorCode => errorCode === '0' ? undefined : (errorsConverter[errorCode] || 'broken');
+
+	const _generateToken = (dataWithPassword) => {
+		const dataString = Object.keys(dataWithPassword)
+			.sort((a, b) => a.localeCompare(b))
+			.map(key => dataWithPassword[key])
+			.reduce((acc, item) => `${acc}${item}`, '');
+		const hash = crypto
+			.createHash('sha256')
+			.update(dataString)
+			.digest('hex');
+		return hash;
+	};
+	
+	const generateToken = (data) => {
+		const { Receipt, DATA, Shops, ...restData } = data;
+		const dataWithPassword = { ...restData, Password: process.env.PAYMENT_EACQ_TERMINAL_PASSWORD };
+		return _generateToken(dataWithPassword);
+	};
+
+	const getUrl = method => `${process.env.PAYMENT_EACQ_AND_TEST_URL}/${method}`;
+	const getMarketUrl = method => `${process.env.PAYMENT_TINKOFF_MARKET_URL}/${method}`;
+
+	const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+	const init = async (options) => {
+		try {
+			const response = await axios({
+				method: 'post',
+				url: getUrl('Init'),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				data: options,
+			});
+	
+			const error = getError(response.data.ErrorCode);
+	
+			const d = {
+				error,
+				request: options,
+				response: response.data,
+			};
+			debug(d);
+			options?.log && options.log(d);
+	
+			return {
+				error,
+				request: options,
+				response: response.data,
+			};
+		} catch (error) {
+			return {
+				error,
+				request: options,
+				response: null,
+			};
+		}
+	};
+
 	const sendInit = async (noTokenData) => {
 		const options = {
 			...noTokenData,
@@ -195,13 +302,13 @@ const f = async () => {
 	const BoolExp = await deep.id('@deep-foundation/core', 'BoolExp');
 	const usersId = await deep.id('deep', 'users');
 
-	const BasePayment = await deep.id('@deep-foundation/payment', 'Payment');
-	const BaseObject = await deep.id('@deep-foundation/payment', 'Object');
-	const BaseSum = await deep.id('@deep-foundation/payment', 'Sum');
-	const BasePay = await deep.id('@deep-foundation/payment', 'Pay');
-	const BaseUrl = await deep.id('@deep-foundation/payment', 'Url');
-	const BasePayed = await deep.id('@deep-foundation/payment', 'Payed');
-	const BaseError = await deep.id('@deep-foundation/payment', 'Error');
+	const BasePayment = await deep.id('@deep-foundation/payments', 'Payment');
+	const BaseObject = await deep.id('@deep-foundation/payments', 'Object');
+	const BaseSum = await deep.id('@deep-foundation/payments', 'Sum');
+	const BasePay = await deep.id('@deep-foundation/payments', 'Pay');
+	const BaseUrl = await deep.id('@deep-foundation/payments', 'Url');
+	const BasePayed = await deep.id('@deep-foundation/payments', 'Payed');
+	const BaseError = await deep.id('@deep-foundation/payments', 'Error');
 
 	const {
 		data: [{ id: packageId }],
@@ -439,7 +546,7 @@ const f = async () => {
         const getError = ${getError};
         const getUrl = ${getUrl};
         const _generateToken = ${_generateToken};
-        const generateTestToken = ${generateTestToken}; 
+        const generateToken = ${generateToken}; 
         const init = ${init};
         const sendInit = ${sendInit};
         `;
@@ -598,35 +705,35 @@ const f = async () => {
 		// Types
 
 		const PPayment = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Payment'
 		);
 		const PObject = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Object'
 		);
 		const PSum = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Sum'
 		);
 		const PPay = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Pay'
 		);
 		const PUrl = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Url'
 		);
 		const PPayed = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Payed'
 		);
 		const PError = await deep.id(
-			'@deep-foundation/payment-individual-entity',
+			packageName,
 			'Error'
 		);
 
-		const paymentTreeId = await deep.id(packageName, 'paymentTreeId');
+		const paymentTreeId = await deep.id(packageName, 'paymentTree');
 
 		// Init
 
