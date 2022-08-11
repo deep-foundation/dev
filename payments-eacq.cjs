7f1060239796a3b0921e6d248c4fd7271f174619
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-11 11:46:42 +0600

    Write route hanler as a string

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -965,147 +965,223 @@ const f = async () => {
 
 	console.log({ payInsertHandlerId });
 
-	const port = 5237;
-	const route = '/tinkoff/payment-eacq';
-
-	const tinkoffNotificationhandlerDependencies = `
-	const errorsConverter = ${JSON.stringify(errorsConverter)};
-	const getError = ${getError};
-	const confirm = ${confirm};
-        `;
-
-	const tinkoffNotificationHandler = async (
-		req,
-		res,
-		next,
-		{ deep, require, gql }
-	) => {
-		/* Dependencies placeholder */
-		if (req.Status == 'AUTORIZED') {
-			await confirm({
-				TerminalKey: process.env.PAYMENT_TEST_TERMINAL_KEY,
-				PaymentId: req.PaymentId,
-				Amount: req.Amount,
-				Token: req.Token,
-				Receipt: req.Receipt,
-			});
-		} else if (req.Status == 'CONFIRMED') {
-			await deep.insert({
-				type_id: PPayed,
-				to_id: req.OrderId,
-				in: {
-					data: [
-						{
-							type_id: Contain,
-							from_id: deep.linkId,
+	await deep.insert(
+		{
+			type_id: await deep.id('@deep-foundation/core', 'Port'),
+			number: { data: { value: process.env.PAYMENT_EACQ_AND_TEST_PORT } },
+			in: {
+				data: {
+					type_id: await deep.id('@deep-foundation/core', 'RouterListening'),
+					from: {
+						data: {
+							type_id: await deep.id('@deep-foundation/core', 'Router'),
+							in: {
+								data: {
+									type_id: await deep.id(
+										'@deep-foundation/core',
+										'RouterStringUse'
+									),
+									string: { data: { value: process.env.PAYMENT_EACQ_AND_TEST_ROUTE } },
+									from: {
+										data: {
+											type_id: await deep.id('@deep-foundation/core', 'Route'),
+											out: {
+												data: {
+													type_id: await deep.id(
+														'@deep-foundation/core',
+														'HandleRoute'
+													),
+													to: {
+														data: {
+															type_id: await deep.id(
+																'@deep-foundation/core',
+																'Handler'
+															),
+															from_id: await deep.id(
+																'@deep-foundation/core',
+																'dockerSupportsJs'
+															),
+															in: {
+																data: {
+																	type_id: await deep.id(
+																		'@deep-foundation/core',
+																		'Contain'
+																	),
+																	// from_id: deep.linkId,
+																	from_id: await deep.id('deep', 'admin'),
+																	string: {
+																		data: {
+																			value: 'tinkoffNotificationHandler',
+																		},
+																	},
+																},
+															},
+															to: {
+																data: {
+																	type_id: await deep.id(
+																		'@deep-foundation/core',
+																		'SyncTextFile'
+																	),
+																	string: {
+																		data: {
+																			value: `
+																			async (
+																				req,
+																				res,
+																				next,
+																				{ deep, require, gql }
+																			) => {
+																				const errorsConverter = {
+																					7: 'Покупатель не найден',
+																					53: 'Обратитесь к продавцу',
+																					99: 'Платеж отклонен',
+																					100: 'Повторите попытку позже',
+																					101: 'Не пройдена идентификация 3DS',
+																					102: 'Операция отклонена, пожалуйста обратитесь в интернет-магазин или воспользуйтесь другой картой',
+																					103: 'Повторите попытку позже',
+																					119: 'Превышено кол-во запросов на авторизацию',
+																					191: 'Некорректный статус договора, обратитесь к вашему менеджеру',
+																					1001: 'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+																					1003: 'Неверный merchant ID',
+																					1004: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+																					1005: 'Платеж отклонен банком, выпустившим карту',
+																					1006: 'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+																					1007: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+																					1008: 'Платеж отклонен, необходима идентификация',
+																					1012: 'Такие операции запрещены для этой карты',
+																					1013: 'Повторите попытку позже',
+																					1014: 'Карта недействительна. Свяжитесь с банком, выпустившим карту',
+																					1015: 'Попробуйте снова или свяжитесь с банком, выпустившим карту',
+																					1019: 'Платеж отклонен — попробуйте снова',
+																					1030: 'Повторите попытку позже',
+																					1033: 'Истек срок действия карты. Свяжитесь с банком, выпустившим карту',
+																					1034: 'Попробуйте повторить попытку позже',
+																					1038: 'Превышено количество попыток ввода ПИН-кода',
+																					1039: 'Платеж отклонен — счет не найден',
+																					1041: 'Карта утеряна. Свяжитесь с банком, выпустившим карту',
+																					1043: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+																					1051: 'Недостаточно средств на карте',
+																					1053: 'Платеж отклонен — счет не найден',
+																					1054: 'Истек срок действия карты',
+																					1055: 'Неверный ПИН',
+																					1057: 'Такие операции запрещены для этой карты',
+																					1058: 'Такие операции запрещены для этой карты',
+																					1059: 'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+																					1061: 'Превышен дневной лимит платежей по карте',
+																					1062: 'Платежи по карте ограничены',
+																					1063: 'Операции по карте ограничены',
+																					1064: 'Проверьте сумму',
+																					1065: 'Превышен дневной лимит транзакций',
+																					1075: 'Превышено число попыток ввода ПИН-кода',
+																					1076: 'Платеж отклонен — попробуйте снова',
+																					1077: 'Коды не совпадают — попробуйте снова',
+																					1080: 'Неверный срок действия',
+																					1082: 'Неверный CVV',
+																					1086: 'Платеж отклонен — не получилось подтвердить ПИН-код',
+																					1088: 'Ошибка шифрования. Попробуйте снова',
+																					1089: 'Попробуйте повторить попытку позже',
+																					1091: 'Банк, выпустивший карту недоступен для проведения авторизации',
+																					1092: 'Платеж отклонен — попробуйте снова',
+																					1093: 'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+																					1094: 'Системная ошибка',
+																					1096: 'Повторите попытку позже',
+																					9999: 'Внутренняя ошибка системы',
+																				};
+																				const getError = (errorCode) =>
+																				errorCode === '0' ? undefined : errorsConverter[errorCode] || 'broken';
+																				const getUrl = (method) =>
+																				"${process.env.PAYMENT_EACQ_AND_TEST_URL}" + "/" + method;
+																				const confirm = async (options) => {
+																					try {
+																						const response = await axios({
+																							method: 'post',
+																							url: getUrl('Confirm'),
+																							data: options,
+																						});
+																			
+																						const error = getError(response.data.ErrorCode);
+																			
+																						const d = {
+																							error,
+																							request: options,
+																							response: response.data,
+																						};
+																						debug(d);
+																						options?.log && options.log(d);
+																			
+																						return {
+																							error,
+																							request: options,
+																							response: response.data,
+																						};
+																					} catch (error) {
+																						return {
+																							error,
+																							request: options,
+																							response: null,
+																						};
+																					}
+																				};
+																				if (req.Status == 'AUTORIZED') {
+																					await confirm({
+																						TerminalKey: "${process.env.PAYMENT_TEST_TERMINAL_KEY}",
+																						PaymentId: req.PaymentId,
+																						Amount: req.Amount,
+																						Token: req.Token,
+																						Receipt: req.Receipt,
+																					});
+																				} else if (req.Status == 'CONFIRMED') {
+																					await deep.insert({
+																						type_id: ${PPayed},
+																						to_id: req.OrderId,
+																						in: {
+																							data: [
+																								{
+																									type_id: ${Contain},
+																									from_id: deep.linkId,
+																								},
+																							],
+																						},
+																					});
+																				} else if (req.Status == 'CANCELED') {
+																					await deep.insert({
+																						type_id: ${PError},
+																						to_id: req.OrderId,
+																						in: {
+																							data: [
+																								{
+																									type_id: ${Contain},
+																									from_id: deep.linkId,
+																									string: { data: { value: getError(req.ErrorCode) } },
+																								},
+																							],
+																						},
+																					});
+																				}
+																				res.send('ok');
+																			};
+																			`
+																		},
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
 						},
-					],
+					},
 				},
-			});
-		} else if (req.Status == 'CANCELED') {
-			await deep.insert({
-				type_id: PError,
-				to_id: req.OrderId,
-				in: {
-					data: [
-						{
-							type_id: Contain,
-							from_id: deep.linkId,
-							string: { data: { value: getError(req.ErrorCode) } },
-						},
-					],
-				},
-			});
+			},
+		},
+		{
+			name: 'INSERT_HANDLE_ROUTE_HIERARCHICAL',
 		}
-		res.send('ok');
-	};
-
-	// await deep.insert(
-	// 	{
-	// 		type_id: await deep.id('@deep-foundation/core', 'Port'),
-	// 		number: { data: { value: port } },
-	// 		in: {
-	// 			data: {
-	// 				type_id: await deep.id('@deep-foundation/core', 'RouterListening'),
-	// 				from: {
-	// 					data: {
-	// 						type_id: await deep.id('@deep-foundation/core', 'Router'),
-	// 						in: {
-	// 							data: {
-	// 								type_id: await deep.id(
-	// 									'@deep-foundation/core',
-	// 									'RouterStringUse'
-	// 								),
-	// 								string: { data: { value: route } },
-	// 								from: {
-	// 									data: {
-	// 										type_id: await deep.id('@deep-foundation/core', 'Route'),
-	// 										out: {
-	// 											data: {
-	// 												type_id: await deep.id(
-	// 													'@deep-foundation/core',
-	// 													'HandleRoute'
-	// 												),
-	// 												to: {
-	// 													data: {
-	// 														type_id: await deep.id(
-	// 															'@deep-foundation/core',
-	// 															'Handler'
-	// 														),
-	// 														from_id: await deep.id(
-	// 															'@deep-foundation/core',
-	// 															'dockerSupportsJs'
-	// 														),
-	// 														in: {
-	// 															data: {
-	// 																type_id: await deep.id(
-	// 																	'@deep-foundation/core',
-	// 																	'Contain'
-	// 																),
-	// 																// from_id: deep.linkId,
-	// 																from_id: await deep.id('deep', 'admin'),
-	// 																string: {
-	// 																	data: {
-	// 																		value: 'tinkoffNotificationHandler',
-	// 																	},
-	// 																},
-	// 															},
-	// 														},
-	// 														to: {
-	// 															data: {
-	// 																type_id: await deep.id(
-	// 																	'@deep-foundation/core',
-	// 																	'SyncTextFile'
-	// 																),
-	// 																string: {
-	// 																	data: {
-	// 																		value: tinkoffNotificationHandler
-	// 																			.toString()
-	// 																			.replace(
-	// 																				'/* Dependencies placeholder */',
-	// 																				tinkoffNotificationhandlerDependencies
-	// 																			),
-	// 																	},
-	// 																},
-	// 															},
-	// 														},
-	// 													},
-	// 												},
-	// 											},
-	// 										},
-	// 									},
-	// 								},
-	// 							},
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// 	{
-	// 		name: 'INSERT_HANDLE_ROUTE_HIERARCHICAL',
-	// 	}
-	// );
+	);
 
 	{
 		// Tests
