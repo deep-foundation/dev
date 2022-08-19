86f4ce079f4f25a0d344c1480dce524616c92b7e
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-19 14:53:51 +0600

    Wait more

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -1434,7 +1434,7 @@ async (
 		const testConfirm = async () => {
 			console.log('testConfirm-start');
 			await testFinishAuthorize();
-			sleep(12000);
+			sleep(25000);
 			let { data } = await deep.select({
 				type_id: PPayed,
 			});
