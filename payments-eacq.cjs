b8dedc87045543126604566743c66168cc9d6cee
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-20 12:08:56 +0600

    Add removeCustomer

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -483,6 +483,33 @@ const getCustomer = async (options) => {
   }
 };
 
+const removeCustomer = async (options) => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: getUrl('RemoveCustomer'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: options,
+    });
+
+    const error = getError(response.data.ErrorCode);
+
+    return {
+      error,
+      request: options,
+      response: response.data,
+    };
+  } catch (error) {
+    return {
+      error,
+      request: options,
+      response: null,
+    };
+  }
+};
+
 	
 	
 
