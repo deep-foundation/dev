87851f117f560f596764eb1417f73d67928acd12
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-20 11:48:26 +0600

    Add charge

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -400,6 +400,34 @@ const f = async () => {
 		}
 	};
 
+	const charge = async (options) => {
+		try {
+			const response = await axios({
+				method: 'post',
+				url: getUrl('Charge'),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				data: options,
+			});
+	
+			const error = getError(response.data.ErrorCode);
+	
+			return {
+				error,
+				request: options,
+				response: response.data,
+			};
+		} catch (error) {
+			return {
+				error,
+				request: options,
+				response: null,
+			};
+		}
+	};
+	
+
 	const addCustomer = async (options) => {
 		try {
 			const response = await axios({
