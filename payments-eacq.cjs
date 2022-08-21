73e0e4b3147bad072b8cb4f491127719093474a2
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-21 11:51:57 +0000

    Update tests

--- payments-eacq.cjs
+++ payments-eacq.cjs
@@ -1849,42 +1849,9 @@ async (
 			console.log('testCancel-start');
 			const testCancelAfterPayBeforeConfirmFullPrice = async () => {
 				console.log('testCanselAfterPayBeforeConfirmFullPrice-start');
-				const initOptions = {
-					TerminalKey: process.env.PAYMENT_TEST_TERMINAL_KEY,
-					OrderId: uniqid(),
-					CustomerKey: deep.linkId,
-					PayType: 'T',
-					Amount: PRICE,
-					Description: 'Test shopping',
-					Language: 'ru',
-					Recurrent: 'Y',
-					DATA: {
-						Email: process.env.PAYMENT_TEST_EMAIL,
-						Phone: process.env.PAYMENT_TEST_PHONE,
-					},
-					// Receipt: {
-					// 	Items: [{
-					// 		Name: 'Test item',
-					// 		Price: sum,
-					// 		Quantity: 1,
-					// 		Amount: PRICE,
-					// 		PaymentMethod: 'prepayment',
-					// 		PaymentObject: 'service',
-					// 		Tax: 'none',
-					// 	}],
-					// 	Email: process.env.PAYMENT_TEST_EMAIL,
-					// 	Phone: process.env.PAYMENT_TEST_PHONE,
-					// 	Taxation: 'usn_income',
-					// }
-				};
+				await testFinishAuthorize();
 
-				console.log({ options: initOptions });
-
-				let initResult = await sendInit(initOptions);
-
-				console.log({ initResult });
-
-				expect(initResult.error).to.equal(undefined);
+				await sleep(4000);
 
 				const {data: [{value: {value: url}}]} = await deep.select({
 					type_id: PUrl,
