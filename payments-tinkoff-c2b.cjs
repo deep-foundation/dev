1c816878810835c3aadb7dfed29ee74156932c10
FreePhoenix888 <freephoenix888@gmail.com>
2022-09-14 06:34:07 +0000

    Remove unused code

--- payments-tinkoff-c2b.cjs
+++ payments-tinkoff-c2b.cjs
@@ -1997,20 +1997,6 @@ async (
     const callIntegrationTests = async () => {
       const testInit = async ({customerKey} = {customerKey: uniqid()}) => {
         console.log('testInit-start');
-        console.log("REMOVE THIS", {
-          type_id: Payment,
-          object: { data: { value: {orderId: uniqid()} } },
-          from_id: deep.linkId,
-          to_id: storageBusinessLink.id,
-          in: {
-            data: [
-              {
-                type_id: Contain,
-                from_id: deep.linkId,
-              },
-            ],
-          },
-        });
         const {
           data: [{ id: paymentId }],
         } = await deep.insert({
