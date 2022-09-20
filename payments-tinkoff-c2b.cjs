849ee6c7d7e39184a52ba382de01ce4a3b93ea19
FreePhoenix888 <freephoenix888@gmail.com>
2022-09-20 16:22:18 +0000

    userLink -> fromLinkOfPayment

--- payments-tinkoff-c2b.cjs
+++ payments-tinkoff-c2b.cjs
@@ -988,12 +988,12 @@ async ({ deep, require, data: { newLink: payLink } }) => {
 
   const Url = await deep.id("@deep-foundation/payments-tinkoff-c2b", "Url");
 
-  const userLinkSelectQuery = await deep.select({
+  const fromLinkOfPaymentSelectQuery = await deep.select({
     id: paymentLink.from_id
   });
-  if(userLinkSelectQuery.error) { throw new Error(userLinkSelectQuery.error.message); }
-  const userLink = userLinkSelectQuery.data[0];
-  console.log({userLink});
+  if(fromLinkOfPaymentSelectQuery.error) { throw new Error(fromLinkOfPaymentSelectQuery.error.message); }
+  const fromLinkOfPayment = fromLinkOfPaymentSelectQuery.data[0];
+  console.log({fromLinkOfPayment});
 
   const storageBusinessLinkSelectQuery = await deep.select({
     id: paymentLink.to_id
