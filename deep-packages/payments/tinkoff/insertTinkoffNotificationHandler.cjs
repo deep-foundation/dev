d57508e2ddcc97bd6f6d2103e99d539f9aec0561
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-01 13:10:47 +0000

    Put authorized code into if block

--- deep-packages/payments/tinkoff/insertTinkoffNotificationHandler.cjs
+++ deep-packages/payments/tinkoff/insertTinkoffNotificationHandler.cjs
@@ -16,84 +16,82 @@ async (
     return next();
   }
 
-  const reqBody = req.body;
-  console.log({reqBody});
-
-  const TinkoffProvider = await deep.id("${packageName}", "TinkoffProvider");
-  const tinkoffProviderLinkSelectQuery = await deep.select({
-    type_id: TinkoffProvider
-  });
-  if(tinkoffProviderLinkSelectQuery.error) {throw new Error(tinkoffProviderLinkSelectQuery.error.message);}
-  const tinkoffProviderId = tinkoffProviderLinkSelectQuery.data[0].id;
-  console.log({tinkoffProviderId});
-
-  const paymentLinkSelectQuery = await deep.select({
-    object: {value: {_contains: {orderId: req.body.OrderId}}}
-  });
-  if(paymentLinkSelectQuery.error) { throw new Error(paymentLinkSelectQuery.error.message); }
-  const paymentLink = paymentLinkSelectQuery.data[0];
-  console.log({paymentLink});
-  if(!paymentLink) { throw new Error("The payment link associated with the order id " + req.body.OrderId + " is not found."); }
-
-  const {data: mpUpPayment, error: mpUpPaymentSelectQueryError} = await deep.select({
-    up: {
-      parent_id: { _eq: paymentLink.id },
-      tree_id: { _eq: await deep.id("${packageName}", "paymentTree") }
-    }
-  });
-  console.log({mpUpPayment});
-  if(mpUpPaymentSelectQueryError) { throw new Error(mpUpPaymentSelectQueryError.message); }
-
-  const Pay = await deep.id("${packageName}", "Pay");
-  const payLink = mpUpPayment.find(link => link.type_id === Pay);
-  console.log({payLink});
-  if(!payLink) { throw new Error("The pay link associated with payment link " + paymentLink + " is not found.") }
-
-
   if (req.body.Status === 'AUTHORIZED') {
-  const confirm = ${confirm.toString()};
+    const reqBody = req.body;
+    console.log({reqBody});
+  
+    const TinkoffProvider = await deep.id("${packageName}", "TinkoffProvider");
+    const tinkoffProviderLinkSelectQuery = await deep.select({
+      type_id: TinkoffProvider
+    });
+    if(tinkoffProviderLinkSelectQuery.error) {throw new Error(tinkoffProviderLinkSelectQuery.error.message);}
+    const tinkoffProviderId = tinkoffProviderLinkSelectQuery.data[0].id;
+    console.log({tinkoffProviderId});
+  
+    const paymentLinkSelectQuery = await deep.select({
+      object: {value: {_contains: {orderId: req.body.OrderId}}}
+    });
+    if(paymentLinkSelectQuery.error) { throw new Error(paymentLinkSelectQuery.error.message); }
+    const paymentLink = paymentLinkSelectQuery.data[0];
+    console.log({paymentLink});
+    if(!paymentLink) { throw new Error("The payment link associated with the order id " + req.body.OrderId + " is not found."); }
+  
+    const {data: mpUpPayment, error: mpUpPaymentSelectQueryError} = await deep.select({
+      up: {
+        parent_id: { _eq: paymentLink.id },
+        tree_id: { _eq: await deep.id("${packageName}", "paymentTree") }
+      }
+    });
+    console.log({mpUpPayment});
+    if(mpUpPaymentSelectQueryError) { throw new Error(mpUpPaymentSelectQueryError.message); }
+  
+    const Pay = await deep.id("${packageName}", "Pay");
+    const payLink = mpUpPayment.find(link => link.type_id === Pay);
+    console.log({payLink});
+    if(!payLink) { throw new Error("The pay link associated with payment link " + paymentLink + " is not found.") }
+    const confirm = ${confirm.toString()};
 
-  const storageReceiverLinkSelectQuery = await deep.select({
-    id: paymentLink.to_id
-  });
-  if(storageReceiverLinkSelectQuery.error) {throw new Error(storageReceiverLinkSelectQuery.error.message);}
-  const storageReceiverId = storageReceiverLinkSelectQuery.data[0].id;
-  console.log({storageReceiverId});
+    const storageReceiverLinkSelectQuery = await deep.select({
+      id: paymentLink.to_id
+    });
+    if(storageReceiverLinkSelectQuery.error) {throw new Error(storageReceiverLinkSelectQuery.error.message);}
+    const storageReceiverId = storageReceiverLinkSelectQuery.data[0].id;
+    console.log({storageReceiverId});
 
-  const Token = await deep.id("${packageName}", "Token");
-  const tokenLinkSelectQuery = await deep.select({
-    type_id: Token,
-    from_id: storageReceiverId,
-    to_id: storageReceiverId
-  });
-  if(tokenLinkSelectQuery.error) {throw new Error(tokenLinkSelectQuery.error.message);}
-  const tokenLink = tokenLinkSelectQuery.data[0];
-  console.log({tokenLink});
+    const Token = await deep.id("${packageName}", "Token");
+    const tokenLinkSelectQuery = await deep.select({
+      type_id: Token,
+      from_id: storageReceiverId,
+      to_id: storageReceiverId
+    });
+    if(tokenLinkSelectQuery.error) {throw new Error(tokenLinkSelectQuery.error.message);}
+    const tokenLink = tokenLinkSelectQuery.data[0];
+    console.log({tokenLink});
 
-    const confirmOptions = {
-      TerminalKey: tokenLink.value.value,
-      PaymentId: req.body.PaymentId,
-      Amount: req.body.Amount,
-      // Receipt: req.body.Receipt,
-    };
-    console.log({confirmOptions});
+      const confirmOptions = {
+        TerminalKey: tokenLink.value.value,
+        PaymentId: req.body.PaymentId,
+        Amount: req.body.Amount,
+        // Receipt: req.body.Receipt,
+      };
+      console.log({confirmOptions});
 
-    const confirmResult = await confirm(confirmOptions);
-    console.log({confirmResult});
+      const confirmResult = await confirm(confirmOptions);
+      console.log({confirmResult});
 
-    if (confirmResult.error) {
-      const errorMessage = "Could not confirm the pay. " + confirmResult.error;
-      const {error: errorLinkInsertError} = await deep.insert({
-        type_id: (await deep.id("${packageName}", "Error")),
-        from_id: tinkoffProviderId,
-        to_id: payLink.id,
-        string: { data: { value: errorMessage } },
-      });
-      if(errorLinkInsertError) { throw new Error(errorLinkInsertError); }
-      throw new Error(errorMessage);
-    }
+      if (confirmResult.error) {
+        const errorMessage = "Could not confirm the pay. " + confirmResult.error;
+        const {error: errorLinkInsertError} = await deep.insert({
+          type_id: (await deep.id("${packageName}", "Error")),
+          from_id: tinkoffProviderId,
+          to_id: payLink.id,
+          string: { data: { value: errorMessage } },
+        });
+        if(errorLinkInsertError) { throw new Error(errorLinkInsertError); }
+        throw new Error(errorMessage);
+      }
 
-    return confirmResult;
+      return confirmResult;
   } else if (req.body.Status === 'CONFIRMED') {
     ${onConfirmedCode}
   } 
