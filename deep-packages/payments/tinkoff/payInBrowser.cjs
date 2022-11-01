2575d435f276bae2a4a15e7304c590722bfb340c
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-01 16:59:44 +0000

    Fix

--- deep-packages/payments/tinkoff/payInBrowser.cjs
+++ deep-packages/payments/tinkoff/payInBrowser.cjs
@@ -3,7 +3,32 @@ const promptSync = require('prompt-sync');
 const prompt = promptSync({ sigint: true });
 
 const payInBrowser = async ({ page, browser, url }) => {
+
+
+
+  // // Если можно удалить уже ранее добавленную карту
+  // await sleep(300);
+  // const removeCardIcon = await page.evaluate(() => {
+  //   return !!document.querySelector(
+  //     'tui-svg[class="ng-star-inserted"]'
+  //   );
+  // });
+  // if (removeCardIcon) {
+  //   await page.click('tui-svg[class="ng-star-inserted"]');
+  // }
+
   await page.goto(url, { waitUntil: 'networkidle2' });
+
+  await sleep(300);
+  await page.evaluate(() => {
+    const saveCardTextDiv = [...document.querySelectorAll("div.t-content")].find(element => element.innerText == "Сохранить карту ");
+    if(saveCardTextDiv) {
+      const saveCardInput = saveCardTextDiv.parentElement.querySelector("input");
+      if(saveCardInput.checked) {saveCardInput.click()}
+    }
+  });
+
+
   await sleep(5000);
   const oldForm = await page.evaluate(() => {
     return !!document.querySelector(
@@ -12,6 +37,7 @@ const payInBrowser = async ({ page, browser, url }) => {
   });
   if (oldForm) {
     console.log('OLD FORM!!!!!!!');
+    
     // Старая форма используется на тестовом сервере
     const cvc1 = await page.evaluate(() => {
       return !!document.querySelector(
@@ -69,11 +95,18 @@ const payInBrowser = async ({ page, browser, url }) => {
       await sleep(300);
       await page.click('button[automation-id="pay-wallet__submit"]'); // submit button
       await sleep(300);
-      await page.waitForSelector('input[name="password"]');
-      const code = prompt('enter code ');
-      console.log('code', code);
-      await page.type('input[name="password"]', code);
-      await sleep(1000);
+      // const ifPasswordExist = await page.waitForSelector(() => {
+      //   return !!document.querySelector(
+      //     'input[name="password"]'
+      //   );
+      // });
+      // if (ifPasswordExist) {
+        await page.waitForSelector('input[name="password"]');
+        const code = prompt('enter code ');
+        console.log('code', code);
+        await page.type('input[name="password"]', code);
+        await sleep(1000);
+      // }
     }
     // TODO: пока старая форма вызывалась только на тестовой карте, где ввод смс кода не нужен
     await sleep(1000);
