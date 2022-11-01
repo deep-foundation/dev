2158cba03410470b1689a79a3beb0f13cbe9b78f
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-01 12:45:48 +0000

    Fix typo

--- deep-packages/payments/tinkoff/insertTinkoffNotificationHandler.cjs
+++ deep-packages/payments/tinkoff/insertTinkoffNotificationHandler.cjs
@@ -2,7 +2,7 @@ const {insertNotificationHandler: baseInsertNotificationHandler} = require("../.
 const {handlersDependencies} = require("./handlersDependencies.cjs");
 const {confirm} = require("./confirm.cjs");
 
-const insertTinkoffNotificationHandler = async ({packageName, packageId, deep, notificationPort, notificationRoute, portTypeId, routerListeningTypeId, routerTypeId, routerStringUseTypeId, routeTypeId, handleRouteTypeId, handlerTypeId, supportsId, containTypeId,  adminId, fileTypeId, onCofirmedCode}) => {
+const insertTinkoffNotificationHandler = async ({packageName, packageId, deep, notificationPort, notificationRoute, portTypeId, routerListeningTypeId, routerTypeId, routerStringUseTypeId, routeTypeId, handleRouteTypeId, handlerTypeId, supportsId, containTypeId,  adminId, fileTypeId, onConfirmedCode}) => {
     const code = `
 async (
   req,
@@ -95,7 +95,7 @@ async (
 
     return confirmResult;
   } else if (req.body.Status === 'CONFIRMED') {
-    ${onCofirmedCode}
+    ${onConfirmedCode}
   } 
   res.send('ok');
 };
