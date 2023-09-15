54b2ea02230caa9eda4519be7c40fc82b14183db
Konstantin Dyachenko <drakonard@gmail.com>
2023-09-15 08:01:56 +0300

    beautify

--- wait-for-prebuild.sh
+++ wait-for-prebuild.sh
@@ -1,8 +1,9 @@
-echo "=========================================="
+echo "==================================================================================================="
 echo ""
-echo "Looks like prebuild is not ready yet. Maybe there is a problem with GitPod prebuilds. In that case just wait it to be finished."
+echo "Looks like prebuild is not ready yet or GitPod was unable to pull prebuild from the cloud."
+echo "In this case, be patient and wait until the prebuild is completed in another terminal just for you."
 echo ""
-echo "=========================================="
+echo "==================================================================================================="
 echo ""
 
 printf "Waiting for prebuild"
