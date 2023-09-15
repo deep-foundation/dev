bfc2a1108fc8e2d51945001f02a78aed4ca29fbf
Konstantin Dyachenko <drakonard@gmail.com>
2023-09-15 04:23:56 +0000

    Waiting for prebuild just in case GitPod failed as again

---
+++ wait-for-prebuild.sh
@@ -0,0 +1,18 @@
+echo "=========================================="
+echo ""
+echo "Looks like prebuild is not ready yet. Maybe there is a problem with GitPod prebuilds. In that case just wait it to be finished."
+echo ""
+echo "=========================================="
+echo ""
+
+printf "Waiting for prebuild"
+
+until [ -f /tmp/prebuild-is-ready.txt ]
+do
+     sleep 5
+     printf "."
+done
+
+echo ""
+echo "Prebuild is ready."
+exit
