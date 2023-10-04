c49bf2e67e0a5aa2443981532e2dbfe7eafede9d
Konstantin Dyachenko <drakonard@gmail.com>
2023-10-04 19:05:29 +0000

    attempt to fix

--- gulpfile.ts
+++ gulpfile.ts
@@ -129,7 +129,13 @@ gulp.task('packages:ci', async () => {
   const packages = Object.keys(modules);
   for (let i = 0; i < packages.length; i++) {
     const currentPackage = process.platform === 'win32' ? packages[i].replace('/', '\\') : packages[i];
-    if (!currentPackage.includes('python-docker-isolation-provider') && !currentPackage.includes('workflows')) parts.push(`echo "${currentPackage} npm ci" && cd ${__dirname}${delimetr}${currentPackage} && npm ci`);
+    if (
+      !currentPackage.includes('deepcase') &&
+      !currentPackage.includes('python-docker-isolation-provider') &&
+      !currentPackage.includes('workflows')
+    ) {
+      parts.push(`echo "${currentPackage} npm ci" && cd ${__dirname}${delimetr}${currentPackage} && npm ci`);
+    }
   }
   const command = parts.join(` && cd ..${delimetr}.. && `);
   console.log('command', command);
